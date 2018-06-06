2901f1cb264b11667823d57349f77410c9c52a47
Juhani <mjluot@utu.fi>
2018-06-06 20:17:57 +0300

    comms & sentence_mode working

--- tokenizer.py
+++ tokenizer.py
@@ -14,8 +14,71 @@ set_session(tf.Session(config=config))
 from keras.models import load_model
 
 
+def data_matrix_to_conllu_tree(x, x_strides, y, vocab, f=sys.stdout):
 
+    #make a reverse vocabulary
+    rev_vocab = {v:k for k,v in vocab.items()}
+    out = ''
+    cntr = 1
 
+    if len(y) < 1:
+        return 
+
+    y = np.array(y).argmax(-1)
+
+    current_token = ''
+    current_sent = []
+    sents = []
+
+    def write_conllu(sent, f):
+        
+        for i, t in enumerate(sent):
+            line = [str(i+1), t.lstrip(' ')]
+            line.extend(['_']*7)
+            
+            '''
+            if i > 0:
+                line[6]='1'
+            else:
+                line[6]='0'
+            '''
+
+            if len(sent)-2 > i:
+                if sent[i+1] == ' ':
+                    line.append('SpaceAfter=False')
+                else:
+                    line.append('_')
+            else:
+                line.append('_')
+            f.write('\t'.join(line)+'\n')
+        f.write('\n')
+
+    for a, line in enumerate(x):
+        for b, char in enumerate(line):
+
+            try:
+                current_token += x_strides[a][b]
+            except: pass
+
+            if y[a][b] == 1.0:
+                if len(current_token.lstrip(' ')) > 0:
+                    current_sent.append(current_token)
+                current_token = ''
+
+            if b > -1 and y[a][b] == 2.0:
+
+                if len(current_token.lstrip(' ')) > 0:
+                    current_sent.append(current_token)
+                current_token = ''
+
+                #write_conllu(current_sent,f)
+                #current_sent = []
+                #cntr = 1
+    else:
+        if current_sent:
+            pass#write_conllu(current_sent,f)
+
+    write_conllu(current_sent,f)
 
 
 def data_matrix_to_conllu(x, x_strides, y, vocab, f=sys.stdout):
@@ -25,7 +88,10 @@ def data_matrix_to_conllu(x, x_strides, y, vocab, f=sys.stdout):
     out = ''
     cntr = 1
 
-    y = y.argmax(-1)
+    if len(y) < 1:
+        return 
+
+    y = np.array(y).argmax(-1)
 
     current_token = ''
     current_sent = []
@@ -80,13 +146,18 @@ def data_matrix_to_conllu(x, x_strides, y, vocab, f=sys.stdout):
             write_conllu(current_sent,f)
 
 
-def make_data_matrix(x, width = 150, vocab=None):
+def make_data_matrix(x, width = 150, vocab=None, train=False):
 
     x_strides = []
     y_1_str = []
     y_2_str = []
 
-    for idx in range(0,len(x), width):
+    if train:
+        xxwidth = 25
+    else:
+        xxwidth = width
+
+    for idx in range(0,len(x), xxwidth):
         x_strides.append(x[idx:idx+width])
 
     #Build vocab
@@ -136,15 +207,24 @@ def tokenize_text(txt,model,vocab, sentence_mode=False):
 
                 curr_block = []
                 buff.write(line + '\n')
+            else:
+                buff.write(line + '\n')
+
+
         else:
 
             if sentence_mode:
 
-                xx, x_strides = make_data_matrix(list(line.strip()), vocab=vocab)
+                if len(line.strip()) > 0:
 
-                pred = model.predict(xx)
-                data_matrix_to_conllu(xx, x_strides, pred, vocab, f=buff)
-                curr_block = []
+                    xx, x_strides = make_data_matrix(list(line.strip()), vocab=vocab)
+
+                    ##
+
+
+                    pred = model.predict(xx)
+                    data_matrix_to_conllu_tree(xx, x_strides, pred, vocab, f=buff)
+                    curr_block = []
 
             else:
                 curr_block.append(line.strip())
@@ -165,21 +245,13 @@ if __name__ == '__main__':
     vocab = pickle.load(open('vocab.pickle','rb'))
 
 
-    test_txt = '''Ruotsin paras "suomalainen" yleisurheilija Daniel Ståhl pettyi pahasti Rooman Timanttiliigan kilpailussa torstai-iltana. Ståhl jäi itselleen vaatimattomalla tuloksella 64,84 neljänneksi.
+    test_txt = '''
+###C:xxx
+###C:lol
+minulla on koira. Minulla on myös kissa.
+kakkakikkare gshsrhrh  aggwgt . aeaeae'''
 
-Edelle kiilasivat Jamaikan Fedrick Dacres (68,51), Liettuan Andrius Gudzius (68,17) ja Iranin Ehsan Hadadi (65,93).
-
-###C:ebin
-
-Viime vuonna MM-hopeaa voittaneen Ståhlin ennätys on 71,29.
-
-Tukholmassa asuva Ståhl puhuu sujuvaa suomea.
-
-- Se on se suomalainen sisu ruotsalaisessa miehessä. Kun menee huonosti, kiroilen suomeksi, Ståhl kertoi viime vuonna Iltalehden haastattelussa.
-
-Ståhlin äiti Taina Laakso on suomalainen.!?'''
-
-    print (tokenize_text(test_txt, model, vocab))
+    print (tokenize_text(test_txt, model, vocab, sentence_mode=True))
 
 
 
